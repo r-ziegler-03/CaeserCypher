c00ca2c25db2c9927e280bb2a671afbd901ac9f7
sarah-yoon <89678660+sarah-yoon@users.noreply.github.com>
2025-01-27 13:26:31 -0800

    Add files via upload

--- Ceasar Cipher/CaesarCipher/CaesarCipher.cs
+++ Ceasar Cipher/CaesarCipher/CaesarCipher.cs
@@ -1,26 +1,35 @@
-﻿// using System.Text;
-//
-// namespace CeasarCipher;
-//
-// public class CaesarCipher
-// {
-//     public static string Encode(string message, int shift)
-//     {
-//         string alphabet = "abcdefghijklmnopqrstuvwxyz";
-//         string newMessage = "";
-//
-//         foreach (char letter in message)
-//         {
-//             if (alphabet.Contains(letter))
-//             {
-//                 int nextLetter = alphabet.IndexOf(letter) + shift;
-//                 newMessage += alphabet[nextLetter];
-//             }
-//             else
-//             {
-//                 newMessage += letter;
-//             }
-//         }
-//         return newMessage;
-//     }
-// }
+﻿using System.Text;
+
+namespace CaesarCipher;
+
+public class CaesarCipher
+{
+    public static string Encode(string message, int shift)
+    {
+        //string of the alphabet
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        //variable to store the encoded message
+        string newMessage = "";
+
+        //loops through each letter in the message
+        foreach (char letter in message)
+        {
+            //if the letter is in the alphabet..
+            if (alphabet.Contains(letter))
+            {
+                //get the position of the shifted letter
+                int shiftPos = alphabet.IndexOf(letter) + shift;
+                //add that letter to the new message
+                newMessage += alphabet[shiftPos];
+            }
+            //if it isn't in the alphabet
+            else
+            {
+                //dont shift it and just add it to the message 
+                newMessage += letter;
+            }
+        }
+        //return the new message
+        return newMessage;
+    }
+}
