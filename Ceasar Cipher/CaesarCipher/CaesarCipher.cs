9d353831b986b50d2c1943e999201a4acfda4a5c
sarah-yoon <89678660+sarah-yoon@users.noreply.github.com>
2025-01-27 13:14:01 -0800

    Add files via upload

---
+++ Ceasar Cipher/CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,26 @@
+﻿// using System.Text;
+//
+// namespace CeasarCipher;
+//
+// public class CaesarCipher
+// {
+//     public static string Encode(string message, int shift)
+//     {
+//         string alphabet = "abcdefghijklmnopqrstuvwxyz";
+//         string newMessage = "";
+//
+//         foreach (char letter in message)
+//         {
+//             if (alphabet.Contains(letter))
+//             {
+//                 int nextLetter = alphabet.IndexOf(letter) + shift;
+//                 newMessage += alphabet[nextLetter];
+//             }
+//             else
+//             {
+//                 newMessage += letter;
+//             }
+//         }
+//         return newMessage;
+//     }
+// }
